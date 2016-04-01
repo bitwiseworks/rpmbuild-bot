a6bcccea81f171a4dd4f73f625b54102b5bbeeea
Dmitriy Kuminov <coding@dmik.org>
2016-04-01 20:17:06 +0000

    rpmbuild-bot: Understand uppercase .CMD extension in REXX wrapper.
    4OS2 is known to add .CMD if a REXX script is called w/o the extension.
    
    
    
    git-svn-id: http://svn.netlabs.org/repos/rpm/rpmbuild-bot@727 978522c7-42b7-df11-88a9-00e081727c95

--- rpmbuild-bot.cmd
+++ rpmbuild-bot.cmd
@@ -1,7 +1,7 @@
-/**/
+/*REXX*/
 parse source . . script
 parse arg args
-if (right(script, 4) == '.cmd') then script = left(script, length(script)-4)||'.sh'
+if (translate(right(script, 4)) == '.CMD') then script = left(script, length(script)-4)||'.sh'
 else script = script||'.sh'
 script = translate(script, '/', '\')
 if (args \== '') then script = script||' '||args
