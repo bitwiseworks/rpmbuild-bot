08d74f7885dfdc2a08efa37247f14f2080fc86e9
Herwig Bauernfeind <herwig.bauernfeind@bitwiseworks.com>
2016-11-11 16:58:08 +0000

    made args work both with / and \
    git-svn-id: http://svn.netlabs.org/repos/rpm/rpmbuild-bot@887 978522c7-42b7-df11-88a9-00e081727c95

--- rpmbuild-bot.cmd
+++ rpmbuild-bot.cmd
@@ -4,5 +4,6 @@ parse arg args
 if (translate(right(script, 4)) == '.CMD') then script = left(script, length(script)-4)||'.sh'
 else script = script||'.sh'
 script = translate(script, '/', '\')
+args   = translate(args, '/', '\')
 if (args \== '') then script = script||' '||args
 '@sh -c '''script''''
