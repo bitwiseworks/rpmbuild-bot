811fdd0f5a2e9a9d1b950266d299e55ddecf3601
Dmitriy Kuminov <coding@dmik.org>
2019-10-23 13:19:22 +0400

    rpmbuild-bot2: Fixup for 8851848.
    A missing '\n' would cause the legacy package RPM script to not
    add legacy DLL's .dbg files into debuginfo.

--- rpmbuild-bot2.py
+++ rpmbuild-bot2.py
@@ -1152,7 +1152,7 @@ def build_prepare (full_spec, spec_base, spec_aux_dir, source_dir, archs, config
             with open (dbgfilelist, 'w') as l:
               for f in all_files:
                 f = os.path.splitext (f) [0] + '.dbg'
-                l.write (f)
+                l.write (f + '\n')
                 masks.append ('*' + f)
             run_pipe ([[RPM2CPIO_EXE, debug_rpm], [CPIO_EXE, '-idm', ' '.join (masks)]])
           # Put the 'done' mark.
